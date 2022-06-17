2e507bbc8b6e99a27965c9fb8b786bf2627ca319
Kerem ILHAN <kerem7095@gmail.com>
2022-06-17 10:48:23 +0300

    Finished

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import {Container} from './components/styles/Container.styled';
+import Header from './components/Header'
+import Card from './components/Card'
+import {ThemeProvider} from 'styled-components'
+import GlobalStyles from './components/styles/Global';
+import content from './Content'
+import Footer from './components/Footer';
+
+
+function App() {
+
+  const theme = {
+    colors: {
+      header: '#ebfbff',
+      body: '#fff',
+      footer: '#003333'
+    },
+    mobile:'768px'
+  }
+  return (
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <Header />
+        <Container>
+          {
+            content.map((item, index)=>(
+              <Card key={index} item={item} />
+            ))
+          }
+        </Container>
+        <Footer />
+      </>
+    </ThemeProvider>
+  );
+}
+
+export default App;
